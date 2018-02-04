a17f9adb909b825fbf9cfe6780d1d498fa285bd4
Artem Belkov <bestK1ngArthur@aol.com>
2018-02-04 15:11:00 +0300

    Добавил удаление ненужных символов

--- serverApp/parsers.py
+++ serverApp/parsers.py
@@ -68,6 +68,54 @@ class ScheduleParser:
 
     def remove_symbols(self, json):
 
-        # Удаляем ненужные символы.
+        # Удаляем ненужные символы из json'а.
+
+        # Если словарь
+        if isinstance(json, dict):
+
+            for key, item in json.items():
+                if isinstance(item, str):
+                    json[key] = self.remove_symbols_from_item(string=item)
+                elif isinstance(item, dict):
+                    json[key] = self.remove_symbols(json=item)
+                elif isinstance(item, list):
+                    json[key] = self.remove_symbols(json=item)
+                else:
+                    continue
+
+        # Если список
+        elif isinstance(json, list):
+
+            for index, item in enumerate(json):
+                if isinstance(item, str):
+                    json[index] = self.remove_symbols_from_item(string=item)
+                elif isinstance(item, dict):
+                    json[index] = self.remove_symbols(json=item)
+                elif isinstance(item, list):
+                    json[index] = self.remove_symbols(json=item)
+                else:
+                    continue
 
         return json
+
+    def remove_symbols_from_item(self, string):
+
+        # Удаляем ненужные символы из строки.
+
+        # Удаляем "null"
+        string = string.replace("null", "")
+
+        # Удаляем "\xa0"
+        string = string.replace("\xa0", " ")
+
+        # Удаляем больше одного пробела.
+        string = string.replace("  ", " ")
+        string = string.replace("   ", " ")
+
+        # Удаляем пробелы на концах строки.
+        if string[0] == " ":
+            string = string[1:]
+        if string[-1] == " ":
+            string = string[:-1]
+
+        return string
