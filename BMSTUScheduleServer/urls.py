de5c0c1cff9ef1f746ebe2b2544392f7851a5cdb
Artem Belkov <bestK1ngArthur@aol.com>
2018-02-05 15:11:24 +0300

    Добавил админку

--- BMSTUScheduleServer/urls.py
+++ BMSTUScheduleServer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.conf.urls import url
+from django.contrib import admin
 from serverApp.views import MainView
 
 urlpatterns = [
     url(r'^$', MainView.as_view()),
+    url(r'^admin/', admin.site.urls),
 ]
